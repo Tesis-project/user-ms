6c05aeef3a623369c572c0113d880a024b7e2d5a
Alvaro Segovia <alvarosego01@gmail.com>
2024-06-16 12:02:50 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MIKRO_ORM_MODULE_CONFIG } from './database/mikro-orm.module';
+import { UserModule } from './user/user.module';
+ @Module({
+  imports: [
+    // MIKRO_ORM_MODULE_CONFIG
+  UserModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
